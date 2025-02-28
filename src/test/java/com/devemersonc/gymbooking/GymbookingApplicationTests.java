251f3854dd608f9569cdad526896461cdc81f3a3
devEmersonc <dev.emersonc@gmail.com>
2025-02-28 13:52:06 -0300

    first commit

---
+++ src/test/java/com/devemersonc/gymbooking/GymbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devemersonc.gymbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
