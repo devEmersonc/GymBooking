dfe0b5622b5bf03a8a98611ff0aacc3279506d87
devEmersonc <dev.emersonc@gmail.com>
2025-02-28 17:25:42 -0300

    UserController

---
+++ src/main/java/com/devemersonc/gymbooking/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.devemersonc.gymbooking.controller;
+
+import com.devemersonc.gymbooking.dto.UserDTO;
+import com.devemersonc.gymbooking.dto.UserRegisterDTO;
+import com.devemersonc.gymbooking.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        List<UserDTO> users = userService.getUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        UserDTO user = userService.getUser(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/normal-user")
+    public ResponseEntity<String> saveNormalUser(@Valid @RequestBody UserRegisterDTO userRegisterDTO) {
+        userService.saveNormalUser(userRegisterDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuario registrado.");
+    }
+
+    @PostMapping("/admin-user")
+    public ResponseEntity<String> saveAdminUser(@Valid @RequestBody UserRegisterDTO userRegisterDTO) {
+        userService.saveAdminUser(userRegisterDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuario registrado.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@Valid @RequestBody UserRegisterDTO userRegisterDTO, @PathVariable Long id) {
+        userService.updateUser(id, userRegisterDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuario actualizado.");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("Usuario eliminado.");
+    }
+}
