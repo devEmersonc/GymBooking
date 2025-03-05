be90a4ccdcf83b6ed2bbf8e2647197059ae70e81
devEmersonc <dev.emersonc@gmail.com>
2025-03-05 17:58:24 -0300

    v1

--- src/main/java/com/devemersonc/gymbooking/repository/BookingRepository.java
+++ src/main/java/com/devemersonc/gymbooking/repository/BookingRepository.java
@@ -2,8 +2,12 @@ package com.devemersonc.gymbooking.repository;
 
 import com.devemersonc.gymbooking.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
 }
